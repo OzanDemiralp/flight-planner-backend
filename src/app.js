0aab54eeabb050022c642226f88e43bf88f7a533
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2025-12-09 23:04:38 +0300

    connect mongodb on local

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+const dbUrl = process.env.MONGO_URL;
+
 const app = express();
+
+mongoose.connect(dbUrl);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database Connected');
+});
 
 app.get('/test', (req, res) => {
   res.send('Hello!');
