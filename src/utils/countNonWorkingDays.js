66530365b7933ec02ca1606f3c4e47e1cb2fa004
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2025-12-10 13:22:57 +0300

    refactor countNonWorkingDays for dayjs

--- src/utils/countNonWorkingDays.js
+++ src/utils/countNonWorkingDays.js
@@ -1,4 +1,6 @@
-holidays = [
+import dayjs from 'dayjs';
+
+export const holidays = [
   '2026-01-01',
   '2026-04-23',
   '2026-05-01',
@@ -8,27 +10,33 @@ holidays = [
   '2026-10-29',
 ];
 
-function countNonWorkingDays(startDate, endDate, holidays, minNonWorkingDays) {
+export default function countNonWorkingDays(
+  startDate,
+  endDate,
+  holidaysArray,
+  minNonWorkingDays
+) {
+  // normalize start/end to dates
+  let current = dayjs(startDate).startOf('day');
+  const end = dayjs(endDate).startOf('day');
+
+  // normalize holidays to "YYYY-MM-DD" and put in a set
+  const holidaySet = new Set(
+    holidaysArray.map((d) => dayjs(d).format('YYYY-MM-DD'))
+  );
+
   let nonWorkingDaysCount = 0;
-  holidays = holidays.map((d) => new Date(d));
 
-  for (
-    let current = new Date(startDate);
-    current <= new Date(endDate);
-    current.setDate(current.getDate() + 1)
-  ) {
-    const day = current.getDay();
-    const isWeekend = day === 0 || day === 6; // sunday 0 saturday 6
+  // loop from start to end
+  while (current.isSame(end) || current.isBefore(end)) {
+    const isWeekend = current.day() === 0 || current.day() === 6; // sunday = 0 saturday = 6
+    const isHoliday = holidaySet.has(current.format('YYYY-MM-DD'));
 
-    const isHoliday = holidays.some(
-      (h) =>
-        h.getFullYear() === current.getFullYear() &&
-        h.getMonth() === current.getMonth() &&
-        h.getDate() === current.getDate()
-    );
     if (isWeekend || isHoliday) {
       nonWorkingDaysCount++;
     }
+
+    current = current.add(1, 'day');
   }
   return {
     nonWorkingDaysCount,
@@ -36,4 +44,4 @@ function countNonWorkingDays(startDate, endDate, holidays, minNonWorkingDays) {
   };
 }
 
-console.log(countNonWorkingDays('2026-05-15', '2026-05-20', holidays, 3));
+//console.log(countNonWorkingDays('2026-05-15', '2026-05-20', holidays, 3));
