0c762f2e93ac6c68cc5e5f9331d128e841fced56
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2025-12-10 23:28:39 +0300

    add error handler middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { planTrip } from './controllers/tripController.js';
 import { validate } from './middleware/validate.js';
+import { errorHandler } from './middleware/errorHandler.js';
 import { planTripSchema } from './validation/planTripSchema.js';
 
 dotenv.config();
@@ -16,6 +17,9 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Database Connected');
+  app.listen(3000, () => {
+    console.log('Serving on port 3000');
+  });
 });
 
 app.get('/test', (req, res) => {
@@ -24,6 +28,11 @@ app.get('/test', (req, res) => {
 
 app.post('/planTrip', validate(planTripSchema), planTrip);
 
-app.listen(3000, () => {
-  console.log('Serving on port 3000');
+app.all(/(.*)/, (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  });
 });
+
+app.use(errorHandler);
