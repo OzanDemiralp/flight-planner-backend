4180ed2a03a9bd65b3f8cbb72b238737cb3a7cb8
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2025-12-10 23:11:03 +0300

    create joi schema for /planTrip request body

---
+++ src/validation/planTripSchema.js
@@ -0,0 +1,29 @@
+import Joi from 'joi';
+
+export const planTripSchema = Joi.object({
+  departureFrom: Joi.string().trim().min(2).max(50).required(),
+  departureTo: Joi.string().trim().min(2).max(50).required(),
+  returnFrom: Joi.string().trim().min(2).max(50).required(),
+  returnTo: Joi.string().trim().min(2).max(50).required(),
+
+  vacationLength: Joi.number().integer().min(1).max(30).required(),
+
+  minNonWorkingDays: Joi.number().integer().min(0).default(0),
+
+  searchWindow: Joi.object({
+    startDate: Joi.date().required().messages({
+      'date.base': '"searchWindow.startDate" must be a valid date',
+      'any.required': '"searchWindow.startDate" is required',
+    }),
+
+    endDate: Joi.date().greater(Joi.ref('startDate')).required().messages({
+      'date.base': '"searchWindow.endDate" must be a valid date',
+      'date.greater':
+        '"searchWindow.endDate" must be after "searchWindow.startDate"',
+      'any.required': '"searchWindow.endDate" is required',
+    }),
+  }).required(),
+}).options({
+  abortEarly: false, // return all errors
+  stripUnknown: true, // drop random things from request body
+});
