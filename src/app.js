d7529e7734049b47e342ac8e343319be346c05d0
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2025-12-10 23:11:29 +0300

    include validation middleware in /planTrip endpoint

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { planTrip } from './controllers/tripController.js';
+import { validate } from './middleware/validate.js';
+import { planTripSchema } from './validation/planTripSchema.js';
 
 dotenv.config();
 const dbUrl = process.env.MONGO_URL;
@@ -20,7 +22,7 @@ app.get('/test', (req, res) => {
   res.send('Hello!');
 });
 
-app.post('/planTrip', planTrip);
+app.post('/planTrip', validate(planTripSchema), planTrip);
 
 app.listen(3000, () => {
   console.log('Serving on port 3000');
