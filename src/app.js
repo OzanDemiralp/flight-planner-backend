c46c3efe1fcd4d01305a92a2e280a916f08a0453
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2026-01-05 16:40:11 +0300

    add session and middleware into app.js + add authroutes into app.js

--- src/app.js
+++ src/app.js
@@ -4,18 +4,39 @@ import { planTrip } from './controllers/tripController.js';
 import { validate } from './middleware/validate.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { planTripSchema } from './validation/planTripSchema.js';
+import { sessionMiddleware } from './config/session.js';
+import { setupPassport } from './config/passport.js';
+
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 app.use(express.json());
+const FRONTEND_ORIGIN =
+  process.env.NODE_ENV === 'production'
+    ? process.env.FRONTEND_ORIGIN
+    : 'http://localhost:3001';
+
 app.use(
   cors({
-    origin: 'http://localhost:3001',
+    origin: FRONTEND_ORIGIN,
+    credentials: true,
   })
 );
 
+app.set('trust proxy', 1);
+
+// session + passport
+app.use(sessionMiddleware());
+setupPassport();
+app.use(passport.initialize());
+app.use(passport.session());
+
+//routes
 app.get('/test', (req, res) => res.send('Hello!'));
 
 app.post('/planTrip', validate(planTripSchema), planTrip);
+
+app.use('/auth', authRoutes);
 
 app.all(/(.*)/, (req, res) => {
   res.status(404).json({ success: false, message: 'Route not found' });
