0ca953e91c57816c1eb760c96dd25470c52ac51e
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2026-02-05 16:20:34 +0300

    add mongo error logging

--- src/server.js
+++ src/server.js
@@ -6,7 +6,14 @@ import app from './app.js';
 const dbUrl = process.env.MONGO_URL;
 const port = process.env.PORT || 3000;
 
-mongoose.connect(dbUrl);
+mongoose.connect(dbUrl).catch((err) => {
+  console.error('Mongo connection error:', err);
+  process.exit(1);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Mongo runtime error:', err);
+});
 
 mongoose.connection.once('open', () => {
   console.log('Database Connected');
